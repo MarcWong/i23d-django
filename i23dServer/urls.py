c1466d3ae6175813aa4bb6dc0eef147fa8b97422
MarcWong <brucewayne@pku.edu.cn>
2018-07-25 23:33:26 +0800

    django url bug fix(temp)

--- i23dServer/urls.py
+++ i23dServer/urls.py
@@ -19,7 +19,7 @@ from i23d import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^index/', views.index),
+    # url(r'^index/', views.index),
     url(r'^obj/', views.obj),
     url(r'^ply/', views.ply),
 ]
